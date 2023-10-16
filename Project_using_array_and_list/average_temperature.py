dbda053c860e2d6a5038c4a45925d78fe6a5e1b5
thomasjos <x22146962@student.ncirl.ie>
2023-10-16 18:29:06 +0100

    average_temperature calculator

--- Project_using_array_and_list/average_temperature.py
+++ Project_using_array_and_list/average_temperature.py
@@ -1,1 +1,23 @@
-print("hello world")
+
+numberofdays = int(input("numbberof days temperature?:"))
+total = 0
+samplearray = []
+for number in range (numberofdays):
+    numberofdayshightemperature = int(input("DAY"+ str(number+ 1)+"high temperature"))
+    samplearray.append(numberofdayshightemperature)
+    total += samplearray[number]
+average = round(total/numberofdays)
+    
+print ("the average temperature is "+ str(average) + " from the above days")
+
+above = 0
+
+for i in samplearray:
+    if i > average:
+        above += 1
+        
+print(str(above)+ " days above averagae ")
+
+
+    
+
