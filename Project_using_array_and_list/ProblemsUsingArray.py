f28f075b08acf84a7601c0fc48da0867c1798b8c
thomasjos <x22146962@student.ncirl.ie>
2023-10-24 16:40:10 +0100

    find maximum product from array

--- Project_using_array_and_list/ProblemsUsingArray.py
+++ Project_using_array_and_list/ProblemsUsingArray.py
@@ -1,12 +1,17 @@
 # How  to check the number that conatins in the array or not 
 
-import numpy as np
 
-sample_array = np.array([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20])
+def maximum_product(sample_array):
+    max1,max2 = 0,0
+    for num in sample_array:
+        if num > max1:
+            max2 = max1
+            max1 = num
+        elif num > max2:
+            max2 = num
+    return max1 * max2
+arr = [10,12,3, 5, 17, 3, 21]
 
-def find_number(array, number):
-    for i in range(len(array)):
-        if array[i] == number:
-            print(i)
+print(maximum_product(arr))
 
-find_number(sample_array, 20)
+
