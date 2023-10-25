a93a91a077fc493c12f5878c9d1ec91594776ff9
thomasjos <x22146962@student.ncirl.ie>
2023-10-25 19:17:45 +0100

    array without duplicates

--- Project_using_array_and_list/ProblemsUsingArray.py
+++ Project_using_array_and_list/ProblemsUsingArray.py
@@ -68,3 +68,16 @@ def missing_number(arr, n):
     return missing
 
 print(missing_number([1, 2, 3, 4, 6], 6))
+
+# 7) create a new array without duplicates 
+def remove_duplicates(lst):
+    unique_lst = []
+    seen = set()
+    for item in lst:
+        if item not in seen:
+            unique_lst.append(item)
+            seen.add(item)
+    return unique_lst
+ 
+my_list = [1, 1, 2, 2, 3, 4, 5]
+print(remove_duplicates(my_list)) 
