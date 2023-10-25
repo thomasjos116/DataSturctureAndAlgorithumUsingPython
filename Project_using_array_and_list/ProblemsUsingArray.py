a212dcb8f83ff7919ecf4065b86ea0a5c12c52c6
thomasjos <x22146962@student.ncirl.ie>
2023-10-25 21:15:29 +0100

    find all pairs of an integer array whose sum is equal to a given number

--- Project_using_array_and_list/ProblemsUsingArray.py
+++ Project_using_array_and_list/ProblemsUsingArray.py
@@ -81,3 +81,17 @@ def remove_duplicates(lst):
  
 my_list = [1, 1, 2, 2, 3, 4, 5]
 print(remove_duplicates(my_list)) 
+
+#Write a function to find all pairs of an integer array whose sum is equal to a given number. Do not consider commutative pairs.
+
+def pair_sum(myList, sum):
+    result = []
+    for i in range(len(myList)):
+        for j in range (i+1, len(myList)):
+            if myList[i]+myList[j] == sum:
+                result.append(f"{myList[i]}+{myList[j]}")
+    return result
+                
+arr = [2, 4, 3, 5, 6, -2, 4, 7, 8, 9]
+target_sum = 7
+print(pair_sum(arr, target_sum))
